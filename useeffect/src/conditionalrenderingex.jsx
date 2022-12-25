70740dde01844992b19c690ac233558438073ccd
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-25 21:37:51 +0530

    Update conditionalrenderingex.jsx

--- useeffect/src/conditionalrenderingex.jsx
+++ useeffect/src/conditionalrenderingex.jsx
@@ -1,2 +1,5 @@
 {!isSignup && <img src = {icon} className = "login-logo" alt = "as"/> }
 // It appears to be a conditional statement that displays an image element if the isSignup variable is false. The img element has a src attribute that is set to the value of the icon variable, and a className attribute that is set to "login-logo". 
+really important for in between condition rendering so that paste what you want
+<button type='submit' className='auth-btn'>{isSignup ? "Signup" : "Log out"}</button>
+if signup = true paste signup else logout
