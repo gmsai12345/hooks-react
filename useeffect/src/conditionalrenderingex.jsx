ffdb3470d3ca829b33176aa52533762c1617e7ba
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-25 21:19:39 +0530

    Create conditionalrenderingex.jsx

---
+++ useeffect/src/conditionalrenderingex.jsx
@@ -0,0 +1,2 @@
+{!isSignup && <img src = {icon} className = "login-logo" alt = "as"/> }
+// It appears to be a conditional statement that displays an image element if the isSignup variable is false. The img element has a src attribute that is set to the value of the icon variable, and a className attribute that is set to "login-logo". 
